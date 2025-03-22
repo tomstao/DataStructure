59f3e024d2cdca48c49a9d2db8768cdb0d362cb9
tomstao <96sutao@gmail.com>
2025-03-21 22:16:07 -0400

    Stack

---
+++ src/dataStructure/Stack/Stack.java
@@ -0,0 +1,15 @@
+package dataStructure.Stack;
+/*
+- Abstract data type
+- LIFO -- last in, first out.
+- push -- adds an item as the top item on the stack
+- pop -- removes the top item on the stack
+- peel -- gets the top item on the stack without popping it
+- Ideal backing data structure: linked list
+
+ */
+public class Stack {
+
+
+
+}
