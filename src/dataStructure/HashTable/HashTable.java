49b6d433562247cf7d4bbf6003b80a03f472d9e4
Tao Su <96sutao@gmail.com>
2025-04-02 18:51:04 -0400

    hashtable

---
+++ src/dataStructure/HashTable/HashTable.java
@@ -0,0 +1,12 @@
+package dataStructure.HashTable;
+/*
+Abstract data type
+Provide access to data using keys
+Key doesn't have to be an integer
+Consists of key/value pairs -data types don't have to match
+Optimized for retrieval(when you know the key)
+Associative array is one type of hash table
+ */
+public class HashTable {
+
+}
