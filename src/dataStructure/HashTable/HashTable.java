deb4450f2644761954d2ea86ffcb2319629621e3
Tao Su <96sutao@gmail.com>
2025-04-03 13:16:02 -0400

    Stack simple implementation

--- src/dataStructure/HashTable/HashTable.java
+++ src/dataStructure/HashTable/HashTable.java
@@ -1,4 +1,5 @@
 package dataStructure.HashTable;
+
 /*
 Abstract data type
 Provide access to data using keys
@@ -8,5 +9,107 @@ Optimized for retrieval(when you know the key)
 Associative array is one type of hash table
  */
 public class HashTable {
+
+    private StoredEmployee[] hashTable;
+
+    public HashTable() {
+        hashTable = new StoredEmployee[10];
+    }
+
+    public void put(String key, Employee employee) {
+        int hashKey = hashKey(key);
+        if (occupied(hashKey)) {
+            int stopIndex = hashKey;
+            if (hashKey == hashTable.length - 1) {
+                hashKey = 0;
+            } else {
+                hashKey++;
+            }
+
+            while (occupied(hashKey) && hashKey != stopIndex) {
+                hashKey = (hashKey + 1) % hashTable.length;
+            }
+        }
+        if (occupied(hashKey)) {
+            System.out.println("Sorry, key " + hashKey + " already exists");
+        } else {
+            hashTable[hashKey] = new StoredEmployee(key, employee);
+        }
+    }
+
+    public Employee get(String key) {
+        int hashKey = findKey(key);
+        if (hashKey == -1) {
+            return null;
+        }
+
+        return hashTable[hashKey].employee;
+    }
+
+    private int hashKey(String key) {
+        return key.length() % hashTable.length;
+    }
+
+    public void print() {
+        for (int i = 0; i < hashTable.length; i++) {
+            if (hashTable[i] == null) {
+                System.out.println("empty");
+            } else {
+                System.out.println("Position " + i + " : " + hashTable[i].employee);
+            }
+        }
+    }
+
+    private int findKey(String key) {
+        int hashKey = hashKey(key);
+        if (hashTable[hashKey] != null && hashTable[hashKey].key.equals(key)) {
+            return hashKey;
+        }
+
+        int stopIndex = hashKey;
+
+        if (hashKey == hashTable.length - 1) {
+            hashKey = 0;
+        } else {
+            hashKey++;
+            System.out.println("Increment:" + hashKey);
+        }
+
+        for (int i = hashKey; i < hashTable.length; i++) {
+            if (hashKey == hashTable.length - 1) hashKey = 0;
+            if (hashTable[i] == null) continue;
+            if (hashTable[i].key.equals(key)) {
+                return i;
+            }
+        }
+//        while (hashKey != stopIndex && hashTable[hashKey] != null && !hashTable[hashKey].key.equals(key)) {
+//            hashKey = (hashKey + 1) % hashTable.length;
+//            System.out.println("Increment2:" + hashKey);
+//        }
+//
+//        if (hashTable[hashKey] != null && hashTable[hashKey].key.equals(key)) {
+//            return hashKey;
+//        } else {
+//            return -1;
+//        }
+        return -1;
+    }
+
+    public Employee remove(String key) {
+        int hashKey = findKey(key);
+        if (hashKey == -1) {
+            return null;
+        }
+
+        Employee employee = hashTable[hashKey].employee;
+        hashTable[hashKey] = null;
+        return employee;
+
+    }
+
+
+    private boolean occupied(int index) {
+        return hashTable[index] != null;
+    }
 
 }
---
+++ src/dataStructure/HashTable/Main.java
@@ -0,0 +1,30 @@
+package dataStructure.HashTable;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 324);
+        Employee marySmith = new Employee("Mary", "Smith", 452);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 4342);
+        Employee billEnd =  new Employee("Bill", "End", 678);
+        HashTable ht = new HashTable();
+        ht.put( "Jones",janeJones);
+        ht.put( "Doe",johnDoe);
+        ht.put( "Wilson",mikeWilson);
+        ht.put( "Smith",marySmith);
+
+        ht.print();
+
+        System.out.println("Retrieve key wilson: " + ht.get("Wilson"));
+        System.out.println("Retrieve Mary: " + ht.get("Smith"));
+
+        System.out.println("remove two employees: ");
+        System.out.println("Find key wilson: :" + ht.get("Wilson"));
+        ht.remove("Wilson");
+        System.out.println("Find key Smith: " + ht.get("Smith"));
+        ht.remove("Jones");
+
+        ht.print();
+    }
+}
